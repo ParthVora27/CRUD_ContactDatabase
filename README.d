fe16a77a352b098bd2efb88c7f96f120c41431c4
PARTH_VORA <voraparth12389@gmail.com>
2021-07-17 19:59:27 +0530

    CRUD_ContactDatabase readme file

---
+++ README.d
@@ -0,0 +1,29 @@
+# CRUD_ContactDatabase
+-Basic GUI to collect User Data
+
+## Steps to set up the project
+-Download the project files.
+-Run the following command : npm install to install the required dependencies.
+-Run the following command : npm install to install the required dependencies.
+
+## Routes
+-localhost:3000/user -> For inserting user data
+-localhost:3000/user/list -> User database
+
+## Technology Stack
+Frontend:
+-HTML5
+-CSS3
+-Vanilla JS
+-HBS templating engine
+
+Backend:
+-Node.js
+-Express
+
+Database:
+-MongoDB
+
+## Features
+Contact GUI with collects data and prints data                                             
+Saves Data to Database for future reference.
